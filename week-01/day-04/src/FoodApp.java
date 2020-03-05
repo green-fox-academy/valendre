3e09a548cc1c09c130be786216430172287d3a9e
valendre <valendre@gmail.com>
2020-03-05 13:17:54 +0100

    First 3 excersizes of the day

---
+++ week-01/day-04/src/FoodApp.java
@@ -0,0 +1,8 @@
+public class FoodApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ week-01/day-04/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Agi!");
+        System.out.println("Hello Kata!");
+        System.out.println(("Hello Gabriel!"));
+    }
+}
