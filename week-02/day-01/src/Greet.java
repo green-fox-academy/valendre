4d657e265664ab567ce91a121692c956411768a6
valendre <valendre@gmail.com>
2020-03-09 15:27:58 +0100

    The excercise Greet is ready.

---
+++ week-02/day-01/src/Greet.java
@@ -0,0 +1,17 @@
+public class Greet {
+  //  From now on, create the usual class wrapper
+  //  and main method on your own.
+
+  // - Create a string variable named `al` and assign the value `Green Fox` to it
+  // - Create a function called `greet` that greets it's input parameter
+  //     - Greeting is printing e.g. `Greetings dear, Green Fox`
+  // - Greet `al`
+  public static void main(String[] args) {
+    String al = "Green Fox";
+    greet(al);
+  }
+
+  public static void greet(String a) {
+    System.out.println("Greetings dear, " + a);
+  }
+}
