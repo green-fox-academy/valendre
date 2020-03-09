6ffc679d311594827bec74044f5b08299d515b8c
valendre <valendre@gmail.com>
2020-03-09 14:59:07 +0100

    Excercise swap elements is ready.

---
+++ week-02/day-01/src/SwapElements.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SwapElements {
+  // - Create an array variable named `orders`
+  //   with the following content: `["first", "second", "third"]`
+  // - Swap the first and the third element of `orders`
+  public static void main(String[] args) {
+    String[] orders = {"first", "second", "third"};
+    System.out.println(Arrays.toString(orders));
+    String a = orders[0];
+    orders[0]=orders[2];
+    orders[2]=a;
+    System.out.println(Arrays.toString(orders));
+  }
+}
