a280a7b44a40ee0263f6575092153cfb3dee958c
valendre <valendre@gmail.com>
2020-03-12 17:28:59 +0100

    Envelope star is ready for 2XP

---
+++ week-02/day-04/src/EnvelopeStar.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class EnvelopeStar {
+  public static void mainDraw(Graphics graphics) {
+
+    graphics.setColor(Color.WHITE);
+    graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+
+    int steps = 15;
+
+    int rows = 2;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < rows; j++) {
+
+        int patternWidth = WIDTH/2;
+        graphics.setColor(new Color(88, 216, 97));
+
+        for (int l = 1; l < steps; l++) {
+          int distance = l * patternWidth / steps;
+          graphics.drawLine(WIDTH/2, distance, (WIDTH/2)-distance, WIDTH/2);
+        }
+        for (int l = 1; l < steps; l++) {
+          int distance = l * patternWidth / steps;
+          graphics.drawLine(WIDTH/2, WIDTH-distance, (WIDTH/2)-distance, WIDTH/2);
+        }
+        for (int l = 1; l < steps; l++) {
+          int distance = l * patternWidth / steps;
+          graphics.drawLine(WIDTH/2, WIDTH-distance, WIDTH/2+distance, WIDTH/2);
+        }
+        for (int l = 1; l < steps; l++) {
+          int distance = l * patternWidth / steps;
+          graphics.drawLine(WIDTH/2, WIDTH/2-distance, WIDTH-distance, WIDTH/2);
+        }
+      }
+    }
+  }
+
+  // region Don't touch the code below
+  static int WIDTH = 300;
+  static int HEIGHT = 300;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+  //endregion
+}
