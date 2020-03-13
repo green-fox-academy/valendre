8e76c838d6650716970c2a057dd905145a582abb
valendre <valendre@gmail.com>
2020-03-13 11:34:06 +0100

    Something modified

--- week-02/day-03/src/TicTacToe.java
+++ week-02/day-03/src/TicTacToe.java
@@ -1,7 +1,6 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TicTacToe {
@@ -26,6 +25,7 @@ public class TicTacToe {
 
     String winnerIs = "Draw";
     try {
+
 
 
       Path path = Paths.get(filename);
