12965b6bc38b0f85756a21dc7cb733ee468c9cc5
valendre <valendre@gmail.com>
2020-03-15 13:39:41 +0100

    Simplified solution

--- week-02/day-03/src/CopyFile.java
+++ week-02/day-03/src/CopyFile.java
@@ -1,7 +1,5 @@
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class CopyFile {
   // Write a function that copies the contents of a file into another
@@ -9,17 +7,14 @@ public class CopyFile {
   // It should return a boolean that shows if the copy was successful
 
   public static void main(String[] args) {
-    String path = "WriteMultipleLinesFile.txt";
+    String path = "./textFiles/WriteMultipleLinesFile.txt";
     System.out.println("Was the copy successful? " + fileCopier(path));
-
   }
 
   public static boolean fileCopier(String path) {
     boolean succed = false;
     try {
-      Path myPath = Paths.get(path);
-      List<String> myLines = Files.readAllLines(myPath);
-      Files.write(Paths.get("copiedfile.txt"), myLines);
+      Files.copy(Paths.get(path), Paths.get("./textFiles/copiedfile.txt"));
       succed = true;
     } catch (Exception e) {
       succed = false;
