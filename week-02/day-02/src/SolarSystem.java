8e9905232995a7fe35d8008606df9915787899f2
valendre <valendre@gmail.com>
2020-03-16 13:49:50 +0100

    Solar system is ready.

---
+++ week-02/day-02/src/SolarSystem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class SolarSystem{
+  public static void main(String... args){
+    ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+    System.out.println(putSaturn(planetList));
+    // Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+  }
+  public static List<String> putSaturn(List<String>planets){
+
+      if (!planets.contains("Saturn")){
+        planets.add(planets.indexOf("Jupiter")+1,"Saturn");
+      }
+
+    return planets;
+  }
+}
