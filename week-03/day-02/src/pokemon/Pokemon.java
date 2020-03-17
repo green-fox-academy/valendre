7f0c438185068a343d03cb9bf45d86f2cd2062ae
valendre <valendre@gmail.com>
2020-03-17 16:11:17 +0100

    Pokemon is ready.

---
+++ week-03/day-02/src/pokemon/Pokemon.java
@@ -0,0 +1,20 @@
+package pokemon;
+
+public class Pokemon {
+  String name;
+  String type;
+  String effectiveAgainst;
+
+  public Pokemon(String name, String type, String effectiveAgainst) {
+    this.name = name;
+    this.type = type;
+    this.effectiveAgainst = effectiveAgainst;
+  }
+
+  public boolean isEffectiveAgainst(Pokemon anotherPokemon) {
+    return this.effectiveAgainst.equals(anotherPokemon.type);
+  }
+  public String getName() {
+    return this.name;
+  }
+}
