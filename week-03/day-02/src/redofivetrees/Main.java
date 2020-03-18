b8c4f9d427212faecf7ff1ec8b02394d2294e220
valendre <valendre@gmail.com>
2020-03-18 17:36:27 +0100

    5 trees redone.

---
+++ week-03/day-02/src/redofivetrees/Main.java
@@ -0,0 +1,14 @@
+package redofivetrees;
+
+import java.awt.Color;
+
+public class Main {
+  public static void main(String[] args) {
+    Tree oldOak = new Tree("oak", Color.GREEN, 100, "M");
+    Tree giantTree = new Tree("Giant pine", Color.GREEN, 500, "F/M");
+    Tree peachTree = new Tree("Peach tree", Color.GREEN, 10, "M");
+    Tree appleTree = new Tree("Apple tree", Color.GREEN, 25, "F/M");
+    Tree cherryTree = new Tree("Cherry tree", Color.GREEN, 100, "F");
+  }
+
+}
---
+++ week-03/day-02/src/redofivetrees/Tree.java
@@ -0,0 +1,17 @@
+package redofivetrees;
+
+import java.awt.Color;
+
+public class Tree {
+  String type;
+  Color leaf;
+  int age;
+  String sex;
+
+  public Tree(String type, Color leaf, int age, String sex) {
+    this.type = type;
+    this.leaf = leaf;
+    this.age = age;
+    this.sex = sex;
+  }
+}
