5feef9a6f19d1385bcb306ca78dccb15ba166c0c
valendre <valendre@gmail.com>
2020-04-14 15:19:01 +0200

    8th excercise is ready.

---
+++ week-06/day-02/src/Exercise8/Exercise8.java
@@ -0,0 +1,19 @@
+package Exercise8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise8 {
+  public static void main(String[] args) {
+
+    List<Character> characters = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g');
+
+    StringBuilder charactersToString = new StringBuilder();
+    characters.stream()
+        .forEach(charactersToString::append);
+
+    System.out.println(charactersToString);
+
+
+  }
+}
